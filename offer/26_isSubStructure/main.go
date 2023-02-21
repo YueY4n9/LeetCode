f22e2e9b69e435c81ecfbc86cdacd233d69589bd
yueyang <yueyang@powerlaw.ai>
2023-02-21 22:12:19 +0800

    offer(26):isSubStructure

---
+++ offer/26_isSubStructure/main.go
@@ -0,0 +1,13 @@
+package main
+
+// https://leetcode.cn/problems/shu-de-zi-jie-gou-lcof/?envType=study-plan&id=lcof&plan=lcof&plan_progress=j7ivt3c
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSubStructure(A *TreeNode, B *TreeNode) bool {
+	return false
+}
