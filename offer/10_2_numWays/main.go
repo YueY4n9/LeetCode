87da3c900b4982a659a9bffecc9b921708e6169c
yueyang <yueyang@powerlaw.ai>
2023-02-22 21:53:43 +0800

    offer(63):maxProfit

--- offer/10_2_numWays/main.go
+++ offer/10_2_numWays/main.go
@@ -1,1 +1,7 @@
-package _0_2_numWays
+package main
+
+// https://leetcode.cn/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/?envType=study-plan&id=lcof&plan=lcof&plan_progress=j7ivt3c
+
+func numWays(n int) int {
+	return 0
+}
--- offer/63_maxProfit/main.go
+++ offer/63_maxProfit/main.go
@@ -1,1 +1,18 @@
-package _3_maxProfit
+package main
+
+func maxProfit(prices []int) int {
+	profit := 0
+	if len(prices) == 0 {
+		return 0
+	}
+	min := prices[0]
+	for i := range prices {
+		if prices[i]-min > profit {
+			profit = prices[i] - min
+		}
+		if prices[i] < min {
+			min = prices[i]
+		}
+	}
+	return profit
+}
