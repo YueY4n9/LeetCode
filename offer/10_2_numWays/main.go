87b6aaadc40b9d1212dcfb5452a41b8487f9b324
yueyang <yueyang@powerlaw.ai>
2023-02-22 23:30:45 +0800

    offer(10_2):numWays

--- offer/10_2_numWays/main.go
+++ offer/10_2_numWays/main.go
@@ -2,6 +2,13 @@ package main
 
 // https://leetcode.cn/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/?envType=study-plan&id=lcof&plan=lcof&plan_progress=j7ivt3c
 
+var m = make(map[int]int)
+
 func numWays(n int) int {
-	return 0
+	m[0] = 1
+	m[1] = 1
+	for i := 2; i <= n; i++ {
+		m[i] = (m[i-1] + m[i-2]) % 1000000007
+	}
+	return m[n]
 }
